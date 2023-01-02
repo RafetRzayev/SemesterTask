9b4d5b9d329a13cbe7e2bbe09cf069c48b772668
Rafet <rafetr@risk.az>
2023-01-02 16:22:23 +0400

    finish

--- SemesterTask/Program.cs
+++ SemesterTask/Program.cs
@@ -10,23 +10,19 @@ namespace SemesterTask
             #region General methods
 
             var generalMethods = new GeneralMethods();
+
             var createdEmails = generalMethods.CreateEmail("Tiina-liina näide");
-
-            Console.WriteLine($"Created {createdEmails[0]}, {createdEmails[1]}");
-
-            Console.WriteLine();
+            Console.WriteLine($"Created {createdEmails[0]}, {createdEmails[1]}\n");
 
             var accountName = generalMethods.CreateAccountName("Ts-liina näide");
-            Console.WriteLine($"Created {accountName} account name and appended the accountNames.txt file");
+            Console.WriteLine($"Created {accountName} account name and appended the accountNames.txt file\n");
 
             string year = "1995";
             var electionWereHeldAt = generalMethods.ElectionWereHeldAt(year);
             if (electionWereHeldAt)
-                Console.WriteLine($"Election were held at {year}");
+                Console.WriteLine($"Election were held at {year}\n");
             else
-                Console.WriteLine($"Election weren't held at {year}");
-
-            Console.WriteLine();
+                Console.WriteLine($"Election weren't held at {year}\n");
 
             #endregion
 
@@ -40,13 +36,13 @@ namespace SemesterTask
              * for the party name and 5 characters for the number of chairs
              */
             Console.WriteLine($"{"Party name",-25}{"Seats",-5}");
-
             foreach (var supportDetail in supportDetails)
             {
                 Console.WriteLine($"{supportDetail.PartyName,-25}{supportDetail.SupportPercent,-5}");
             }
             Console.WriteLine();
 
+            Console.WriteLine("All possible combinations:");
             statistic.PossibleCombinationOfParties("2022");
             Console.WriteLine();
 
@@ -59,12 +55,11 @@ namespace SemesterTask
             var difference = statistic.DifferencePopularity("2020", "2022", "Keskerakond");
 
             if (difference > 0)
-                Console.WriteLine($"grew {difference}%");
+                Console.WriteLine($"grew {difference}%\n");
             else if (difference < 0)
-                Console.WriteLine($"decrease {-difference}%");
+                Console.WriteLine($"decrease {-difference}%\n");
             else
-                Console.WriteLine($"Doesn't changed support percantage");
-            Console.WriteLine();
+                Console.WriteLine($"Doesn't changed support percantage\n");
 
             statistic.PopularityInRange("2010", "2022");
             Console.WriteLine();
@@ -76,8 +71,7 @@ namespace SemesterTask
             foreach (var partyName in statistic.AllPartyNames())
             {
                 Console.WriteLine(partyName);
-            }
-            
+            }      
             Console.WriteLine();
 
             #endregion
@@ -85,19 +79,21 @@ namespace SemesterTask
             #region 2019 Election analysis
 
             var electionAnalysis = new ParliamentElectionAnalysis();
+
             var tresholdValue = electionAnalysis.ElectoralTresholdValue();
-            Console.WriteLine($"Treshold value: {tresholdValue}");
-            Console.WriteLine();
+            Console.WriteLine($"Treshold value: {tresholdValue}\n");
+
             electionAnalysis.PartiesExceedTresholdValue();
             Console.WriteLine();
+
             var districtNumber = 4;
             var simpleQuota = electionAnalysis.QuotaForElectoralDistrict(districtNumber);
-            Console.WriteLine($"Simple quota for electoral district number {districtNumber} is {simpleQuota}");
-            Console.WriteLine();
+            Console.WriteLine($"Simple quota for electoral district number {districtNumber} is {simpleQuota}\n");
+
             electionAnalysis.DistrictMandators(districtNumber);
             Console.WriteLine();
-            electionAnalysis.Print(districtNumber);
 
+            electionAnalysis.Print(districtNumber);
             Console.WriteLine();
 
             #endregion
